7468f31d5ece291600b37d3f6b5e51513a9b0b17
Jimmy Ho <jimmyho.929@outlook.com>
2021-07-13 23:34:38 -0500

    testing out div method

--- app.js
+++ app.js
@@ -53,19 +53,48 @@ const getMonth = document.querySelector("h1");
 const month = document.querySelector("select");
 
 month.addEventListener("change", (e) => {
-  const orderedList = document.getElementById("list");
-  orderedList.innerHTML = "";
-  getMonth.innerHTML = e.target.value;
-  monthsOfYear.forEach((m) => {
-    if (m.month == getMonth.textContent) {
+getMonth.innerHTML = e.target.value;
+
+/******
+Creating 2 divs
+ */ 
+ 
+
+monthsOfYear.forEach( (m)=> { 
+  if (m.month === e.target.value) {
+     const wrapper = document.getElementById("wrapper");
+      wrapper.innerHTML = `<span id='tips'>Total Tip: 0</span>`;
       for (let i = 0; i < m.days; i++) {
-        const li = document.createElement("li");
-        orderedList.appendChild(li);
-        const input = document.createElement("input");
-        input.type = "number";
-        input.min = "0";
-        li.appendChild(input);
-        document.getElementById('tips').innerHTML = `Total Tip: 0`
+        const div = document.createElement("div");
+        const span = document.getElementById("tips");
+        wrapper.insertBefore(div, span);
+        div.innerHTML = `${i+1}`;
+        div.className = "date";
+        const tipBox = document.createElement("div");
+        div.appendChild(tipBox)
+        tipBox.innerHTML = 'Test'
+        tipBox.className = "tip-box"
+    }
+  }
+})
+
+  /******
+   Creating list item
+   */
+
+  // const orderedList = document.getElementById("list");
+  // orderedList.innerHTML = "";
+  // 
+  // monthsOfYear.forEach((m) => {
+  //   if (m.month == getMonth.textContent) {
+  //     for (let i = 0; i < m.days; i++) {
+        // const li = document.createElement("li");
+        // orderedList.appendChild(li);
+        // const input = document.createElement("input");
+        // input.type = "number";
+        // input.min = "0";
+        // li.appendChild(input);
+        // document.getElementById('tips').innerHTML = `Total Tip: 0`
         // const button = document.createElement('button');
         // button.innerHTML = 'Edit';
         // li.appendChild(button);
@@ -76,8 +105,8 @@ month.addEventListener("change", (e) => {
         //     button.innerHTML = 'Edit';
         //   }
         // })
-      }
-    }
+    //   }
+    // }
   });
   const tipInput = document.querySelectorAll("input");
   const tipArray = []
@@ -98,4 +127,4 @@ month.addEventListener("change", (e) => {
   });
   
   
-});
+// });
