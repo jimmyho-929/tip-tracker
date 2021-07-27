48293b4ca3e98e549cb16ed502d4250ac97ca620
Jimmy Ho <jimmyho.929@outlook.com>
2021-07-27 10:54:16 -0500

    updated color and positioned graph in middle

--- app.js
+++ app.js
@@ -54,10 +54,9 @@ const month = document.querySelector("select");
 
 month.addEventListener("change", (e) => {
   getMonth.innerHTML = e.target.value;
-
-  /******
-Creating 2 divs
- */
+  document.getElementById(
+    "tips"
+  ).innerHTML = `Total Tip: <span style="font-weight:bold">0</span>`
 
   monthsOfYear.forEach((m) => {
     if (m.month === e.target.value) {
@@ -65,7 +64,6 @@ Creating 2 divs
       wrapper.innerHTML = ``;
       for (let i = 0; i < m.days; i++) {
         const div = document.createElement("div");
-        // const span = document.getElementById("tips");
         wrapper.appendChild(div);
         div.innerHTML = `${i + 1}`;
         div.className = "date";
@@ -77,24 +75,13 @@ Creating 2 divs
       const tipInput = [...document.getElementsByClassName("tip-box")];
       const tipArray = [];
       let totalTip = 0;
-      let myChart = document.getElementById('myChart').getContext('2d');
-
-      let barChart = new Chart(myChart, {
-        type: 'bar',
-        data: {
-          labels: 'Tips',
-          datasets: [{
-            label: 'Tip',
-            data: totalTip
-          }]
-        },
-        options: {}
-      }) 
+      
 
       tipInput.forEach((tip) => {
         tip.addEventListener("click", () => {
           tip.value = prompt("Enter tip amount");
           tip.innerHTML = tip.value;
+          tip.style.color = 'black';
           tipArray.push(parseInt(tip.value)); 
           totalTip = 0;
           tipArray.forEach((tip) => {
@@ -102,7 +89,38 @@ Creating 2 divs
             document.getElementById(
               "tips"
             ).innerHTML = `Total Tip: <span style="font-weight:bold">${totalTip}</span>`;
+              
+            let myChart = document.getElementById('myChart').getContext('2d');
+          let barChart = new Chart(myChart, {
+            type: 'bar',
+            data: {
+              labels: ['Tips'],
+              datasets: [{
+                label: 'Tip',
+                data: [totalTip]
+              }]
+            },
+            options: {}
+          }) 
+
+          // barChart.destroy();
+        
+
+        // barChart = new Chart(myChart, {
+        //   type: 'bar',
+        //   data: {
+        //     labels: ['Tips'],
+        //     datasets: [{
+        //       label: 'Tip',
+        //       data: [totalTip]
+        //     }]
+        //   },
+        //   options: {}
+        // }) 
+            
+          
           });
+          
         });
         
       });
